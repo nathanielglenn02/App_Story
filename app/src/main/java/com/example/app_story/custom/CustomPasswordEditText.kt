4ae1ca8d450b268ce9e38727ee034f76de8a3f78
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-11-27 12:17:17 +0700

    CUSTOM EDPASSWORD

---
+++ app/src/main/java/com/example/app_story/custom/CustomPasswordEditText.kt
@@ -0,0 +1,40 @@
+package com.example.app_story.custom
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import com.example.app_story.R
+
+class CustomPasswordEditText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = android.R.attr.editTextStyle
+) : AppCompatEditText(context, attrs, defStyleAttr) {
+
+    init {
+        setupListener()
+    }
+
+    private fun setupListener() {
+        // Tambahkan listener untuk memantau perubahan teks
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                validatePassword(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun validatePassword(password: CharSequence?) {
+        if (password.isNullOrEmpty() || password.length < 8) {
+            error = context.getString(R.string.password_error_message) // Pesan error jika kurang dari 8 karakter
+        } else {
+            error = null // Hapus error jika valid
+        }
+    }
+}
